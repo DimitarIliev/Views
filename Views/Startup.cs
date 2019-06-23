02ec03a8fbafb2f2c42122c06eb037bc93d4a5d9
Димитар Илиев <iliev.dimitar@students.finki.ukim.mk>
2019-06-23 20:32:58 +0200

    Demonstration on how to create a custom view engine, how Razor works by translating CSHTML files into C# classes. Demonstration on how to use Layout sections and Partial views & how to change the locations that Razor uses to locate view files.

--- Views/Startup.cs
+++ Views/Startup.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
+using Views.Infrastructure;
 
 namespace Views
 {
@@ -16,6 +19,14 @@ namespace Views
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.ViewLocationExpanders.Add(new SimpleExpander());
+                options.ViewLocationExpanders.Add(new ColorExpander());
+            });
+            //services.Configure<MvcViewOptions>(options =>
+            //options.ViewEngines.Clear(); clear all the viewengines from the list
+            //options.ViewEngines.Insert(0, new DebugDataViewEngine()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
